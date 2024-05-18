1c4cb767354007819d03d934218730323ce7e550
Knux <knuxfan@live.co.uk>
2024-05-18 12:31:32 +0100

    Delete FP2Archipelago/build.bat

--- FP2Archipelago/build.bat
+++
@@ -1,2 +0,0 @@
-dotnet build
-copy "C:\Users\Knuxf\Documents\GitHub\FP2ArchipelagoMockup\FP2Archipelago\bin\Debug\net35\FP2Archipelago.dll" "D:\Steam\steamapps\common\Freedom Planet 2\BepInEx\plugins\Archipelago"
