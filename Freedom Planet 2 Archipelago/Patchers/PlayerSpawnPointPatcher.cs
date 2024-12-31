385dc1723514ea8b88157384d0bde1ea135edf18
Knux <knuxfan@live.co.uk>
2024-12-31 15:31:33 +0000

    Stop the character randomiser from picking the current character

--- Freedom Planet 2 Archipelago/Patchers/PlayerSpawnPointPatcher.cs
+++ Freedom Planet 2 Archipelago/Patchers/PlayerSpawnPointPatcher.cs
@@ -15,8 +15,20 @@ namespace Freedom_Planet_2_Archipelago.Patchers
             // Also check that we aren't in a Basic Tutorial, as there's versions for each character.
             if ((long)Plugin.SlotData["character"] == 4 && !SceneManager.GetActiveScene().name.Contains("Tutorial1"))
             {
-                // Generate a random number between from 0 to 3 then set the save file's character based on the selected number.
-                switch (Plugin.Randomiser.Next(0, 4))
+                // Get our current character, knocking the number down by one if it's higher than that, as Carol takes up two slots.
+                int currentCharacter = (int)FPSaveManager.character;
+                if (currentCharacter > 1)
+                    currentCharacter--;
+
+                // Roll a number for our character choice.
+                int nextCharacter = Plugin.Randomiser.Next(0, 4);
+
+                // If we've rolled the same number, then roll again.
+                while (nextCharacter == currentCharacter)
+                    nextCharacter = Plugin.Randomiser.Next(0, 4);
+
+                // Set the save file's character based on the selected number.
+                switch (nextCharacter)
                 {
                     case 0: FPSaveManager.character = FPCharacterID.LILAC; break;
                     case 1: FPSaveManager.character = FPCharacterID.CAROL; break;
